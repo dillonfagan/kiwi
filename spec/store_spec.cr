bb39d613dbc9ffc74b4acbebe61b6a5a15e8770b
Dillon Fagan <dillonfagan@pm.me>
2021-10-30 09:26:37 -0400

    Refactor Store tests

--- spec/store_spec.cr
+++ spec/store_spec.cr
@@ -1,65 +1,75 @@
 require "./spec_helper"
 
 describe Kiwi::Store do
-  it "has a name" do
-    store = Kiwi::Store.new("flavors")
+  describe "#new" do
+    it "sets the name" do
+      store = Kiwi::Store.new("flavors")
 
-    store.name.should eq("flavors")
-  end
-
-  it "returns an ID when pushing an entry" do
-    store = Kiwi::Store.new("airlines")
-    id = store.push("PanAm")
-
-    id.should_not be_nil
-  end
-
-  it "raises StoreException with #get if the ID does not exist" do
-    store = Kiwi::Store.new("empty")
-
-    expect_raises(Kiwi::StoreException, "Invalid ID") do
-      store.get(Kiwi::ID.new)
+      store.name.should eq("flavors")
     end
   end
 
-  it "returns the entry's value with #get" do
-    store = Kiwi::Store.new("currencies")
-    id = store.push("DKK")
+  describe "#push" do
+    it "returns an ID" do
+      store = Kiwi::Store.new("airlines")
+      id = store.push("PanAm")
 
-    store.get(id).should eq("DKK")
-  end
-
-  it "raises a StoreException with #patch if the ID does not exist" do
-    store = Kiwi::Store.new("anything")
-
-    expect_raises(Kiwi::StoreException, "Invalid ID") do
-      store.patch(Kiwi::ID.new, "value")
+      id.should_not be_nil
     end
   end
 
-  it "updates an entry's value with #patch" do
-    store = Kiwi::Store.new("colors")
-    id = store.push("orange")
-    store.patch(id, "black")
+  describe "#get" do
+    it "raises StoreException, given invalid ID" do
+      store = Kiwi::Store.new("empty")
 
-    store.get(id).should eq("black")
-  end
+      expect_raises(Kiwi::StoreException, "Invalid ID") do
+        store.get(Kiwi::ID.new)
+      end
+    end
 
-  it "raises a StoreException if #delete takes ID that does not exist" do
-    store = Kiwi::Store.new("anything")
+    it "returns an entry's value" do
+      store = Kiwi::Store.new("currencies")
+      id = store.push("DKK")
 
-    expect_raises(Kiwi::StoreException, "Invalid ID") do
-      store.delete(Kiwi::ID.new)
+      store.get(id).should eq("DKK")
     end
   end
 
-  it "deletes an entry with #delete" do
-    store = Kiwi::Store.new("countries")
-    id = store.push("Estonia")
-    store.delete(id)
+  describe "#patch" do
+    it "raises StoreException, given invalid ID" do
+      store = Kiwi::Store.new("anything")
 
-    expect_raises(Kiwi::StoreException, "Invalid ID") do
-      store.get(id)
+      expect_raises(Kiwi::StoreException, "Invalid ID") do
+        store.patch(Kiwi::ID.new, "value")
+      end
+    end
+
+    it "updates an entry's value" do
+      store = Kiwi::Store.new("colors")
+      id = store.push("orange")
+      store.patch(id, "black")
+
+      store.get(id).should eq("black")
+    end
+  end
+
+  describe "#delete" do
+    it "raises StoreException, given invalid ID" do
+      store = Kiwi::Store.new("anything")
+
+      expect_raises(Kiwi::StoreException, "Invalid ID") do
+        store.delete(Kiwi::ID.new)
+      end
+    end
+
+    it "deletes an entry" do
+      store = Kiwi::Store.new("countries")
+      id = store.push("Estonia")
+      store.delete(id)
+
+      expect_raises(Kiwi::StoreException, "Invalid ID") do
+        store.get(id)
+      end
     end
   end
 end
